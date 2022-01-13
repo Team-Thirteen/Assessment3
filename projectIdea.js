92029b822c985dc710180745accb7ff27576c9ee
Geoffro667 <s3930693@student.rmit.edu.au>
2022-01-13 19:19:35 +1000

    Updated Project idea html

--- projectIdea.js
+++ projectIdea.js
@@ -1,25 +1,29 @@
-function openCity(cityName, elmnt, color) {
-    // Hide all elements with class="tabcontent" by default */
+function openpage(evt, memberName) {
+    // Declare all variables
     var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
+        tabcontent[i].style.display = "none";
     }
-  
-    // Remove the background color of all tablinks/buttons
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].style.backgroundColor = "";
-    }
-  
-    // Show the specific tab content
-    document.getElementById(cityName).style.display = "block";
-  
-    // Add the specific color to the button used to open the tab content
-    elmnt.style.backgroundColor = color;
-  }
-  
-  // Get the element with id="defaultOpen" and click on it
-  document.getElementById("defaultOpen").click();
 
-// I haven't changed anything from this file, just copied and pasted. But it was from the full page tab at the bottom, so might need to copy and paste again.
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(memberName).style.display = "block";
+    evt.currentTarget.className += " active";
+
+    //Hide h1 element onclick of team member name 
+    var x = document.getElementById("projectideaheader"); {
+        x.style.display = "none";
+    }
+    
+   
+}
+
+
